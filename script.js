11e0c6c49092a75fd2f422adc8ddeaf49c2011dd
Linberg07 <63363265+Linberg07@users.noreply.github.com>
2021-08-03 00:29:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+var acord_item = $('.accordion__item');
+var acord_ul = $('.accordion__item_ul');
+var acord_icon = $('.dcjq-parent');
+acord_icon.removeClass('active');
+acord_ul.css('display','none');
+
+acord_item.click(function(){
+	var w = $(window).width();
+	if(w<768){
+	if(acord_icon.hasClass('active')){
+		$('.graphite .accordion a:hover').css('color','#fff');
+	}else{
+		$('.graphite .accordion a:hover').css('color','#ddd');
+	}
+	}
+	
+	
+		
+	acord_ul.css('display','none');
+	var OffsetTop = $(this).offset().top;
+
+	setTimeout(function(){ 
+		
+		window.scrollTo(OffsetTop,OffsetTop);
+		
+		},200);
+	 
+
+
+})
+console.log('1');
+
+function offset(el) {
+    var rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+
+});
